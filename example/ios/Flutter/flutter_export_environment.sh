b047429c04183a539be993d1e42f07065faf6fcc
Ahmed Al-Yousif <46423982+AhmedAlYousif@users.noreply.github.com>
2025-01-14 20:22:09 +0200

    upgrade intl

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=F:\Development\flutter"
-export "FLUTTER_APPLICATION_PATH=F:\Development\auto_direction\example"
+export "FLUTTER_ROOT=/Users/peteraziz/FlutterDev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/peteraziz/Nebu-Systems-Apps/Development/auto_direction/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
